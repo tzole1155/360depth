28380f987bf56f76cb4fa3fc8f0f0b98d09f73fb
Georgios Albanis <giorgos_al10@hotmail.com>
2021-06-03 17:11:34 +0300

    add smooth shading

--- js/loaders/OBJLoader.js
+++ js/loaders/OBJLoader.js
@@ -872,6 +872,14 @@ class OBJLoader extends Loader {
 
 				mesh.name = object.name;
 
+                //smooth function
+                smooth_geom = new Geometry().fromBufferGeometry(mesh.geometry);
+                smooth_geom.mergeVertices();
+                smooth_geom.computeVertexNormals();
+                smooth_geom.computeFaceNormals();
+                mesh.geometry = new THREE.BufferGeometry().fromGeometry(smooth_geom);
+                //
+
 				container.add( mesh );
 
 			}
